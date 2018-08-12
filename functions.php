ecfcc4e2017f3385c5d0f7264234d7bb8d5f966a
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2018-08-12 20:40:21 +0900

    Fix css file enqueue code

--- functions.php
+++ functions.php
@@ -3,26 +3,13 @@
 if ( ! function_exists( 'child_theme_enqueue_styles' ) ) {
     /**
      * 子テーマ用のCSSを読み込み。
-     * 親CSSファイル > 子CSSファイルの順で読み込むよう制御
+     * 親CSSファイル > 子CSSファイルの順で読み込む。
      */
     function child_theme_enqueue_styles() {
-        // デフォルトの子テーマ用CSSファイル登録を解除
-        wp_deregister_style( 'style' );
-
         // 親テーマのCSSファイルの登録
-        wp_register_style(
-            'parent-style',
-            get_template_directory_uri() . '/style.css',
-            array(),
-            filemtime( get_template_directory() . '/style.css' )
-        );
-
-        // 親テーマと子テーマのCSSファイルを読み込み
         wp_enqueue_style(
-            'style',
-            get_theme_file_uri() . '/style.css',
-            array( 'parent-style' ),
-            filemtime( get_theme_file_path( '/style.css' ) )
+            'parent-style',
+            get_template_directory_uri() . '/style.css'
         );
     }
     add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
